eff52da1ae8c8a142fc8878fe4e52b2f93c8fa84
Ben Emanuel <benjaminemanuel13@outlook.com>
2025-05-21 16:40:16 +0100

    Tidy again

--- SemanticAgent.Business/Services/Agent.cs
+++ SemanticAgent.Business/Services/Agent.cs
@@ -21,11 +21,11 @@ namespace SemanticAgent.Business.Services
             kernel = GetKernel();
         }
 
-        public async Task<string> Ask(string question)
+        public async Task<string> Ask(string question = "Check current UTC time and return current weather in Boston city.")
         {
             OpenAIPromptExecutionSettings executionSettings = new() { ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions };
 
-            FunctionResult result = await kernel.InvokePromptAsync("Check current UTC time and return current weather in Boston city.", new(executionSettings));
+            FunctionResult result = await kernel.InvokePromptAsync(question, new(executionSettings));
 
             return result.ToString();
         }
